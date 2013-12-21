877bfa6c44792f0cdaa046c43c24cd13f187944f
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-12-21 15:36:23 +0900

    enable Friendly URLs

--- FriendlyURLsSample/Global.asax.cs
+++ FriendlyURLsSample/Global.asax.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
+using System.Web.Routing;
 
 namespace FriendlyURLsSample
 {
@@ -11,6 +7,7 @@ namespace FriendlyURLsSample
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+          RouteConfig.RegisterRoutes(RouteTable.Routes);  // <- FriendlyURLsを有効に
         }
     }
 }
