bfa99234ab86b6ff6c0ae7a1a71f4cd99597f904
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-12-21 15:47:25 +0900

    create Greeting page

---
+++ FriendlyURLsSample/Greeting.aspx.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.FriendlyUrls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FriendlyURLsSample
+{
+  public partial class Greeting : System.Web.UI.Page
+  {
+    public string Name { get; private set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Name = FriendlyUrl.Segments.First();
+    }
+  }
+}
